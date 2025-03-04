185fb947433b63fbfb541c95acc0cee54a561c84
Abe Pasion <pasionabe@gmail.com>
2025-03-04 12:13:27 -0500

    Create Pages

--- hello-world-render/server.js
+++ hello-world-render/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-    res.send("Hello, World from Render!");
+    res.sendFile(path.join(__dirname, "public", "listing.html"));
 });
 
 app.get("/admin", (req, res) => {
-    res.send("Admin page")
-})
+    res.sendFile(path.join(__dirname, "public", "admin.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
